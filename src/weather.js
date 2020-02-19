e93816eadce80282575b0f621acdbd9006687a8b
Hamza Zaitoun <49735537+DevClick-Hz@users.noreply.github.com>
2020-02-19 22:23:58 +0100

    fixing weather app

--- src/weather.js
+++ src/weather.js
@@ -9,14 +9,15 @@ class weather {
   }
   // weather informations
   async displayweather() {
-    const api = `http://api.weatherstack.com/current?access_key=29f3bdfb6feea8cc6e3565ef190817ba&query=${this.city}`
+    const api = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=4eddfe1ea7e550a2ec14cc5d49a0f913
+    `
     try {
       const response = await fetch(api)
       try {
         const json = await response.json()
-        this.temperature = json.current.temperature
-        this.humidity = json.current.humidity
-        this.wind = json.current.wind_speed
+        this.temperature = json.main.temp
+        this.humidity = json.main.humidity
+        this.wind = json.wind.speed
         document.getElementById("temp").innerHTML = `${this.temperature}&deg; C`
         document.getElementById(
           "city"
@@ -64,16 +65,16 @@ document.getElementById("city-inpt").addEventListener("keyup", function() {
         }
         async displayweather() {
           let userCity = document.getElementById("city-inpt").value
-          const api = `http://api.weatherstack.com/current?access_key=29f3bdfb6feea8cc6e3565ef190817ba&query=${userCity}`
+          const api = `http://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=4eddfe1ea7e550a2ec14cc5d49a0f913`
           try {
             const response = await fetch(api)
             try {
               const json = await response.json()
-              this.temperature = json.current.temperature
-              this.city = json.location.name
-              this.country = json.location.country
-              this.humidity = json.current.humidity
-              this.wind = json.current.wind_speed
+              this.temperature = json.main.temp
+              this.city = json.name
+              this.country = json.sys.country
+              this.humidity = json.main.humidity
+              this.wind = json.wind.speed
               document.getElementById(
                 "temp"
               ).innerHTML = `${this.temperature}&deg; C`
